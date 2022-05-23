4a099b00d91a5b6009ecb8081bc03d63a4427fca
CoderShail09 <95957280+CoderShail09@users.noreply.github.com>
2022-05-23 17:15:08 +0530

    Add files via upload
    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TaskApp from "./components/TaskApp";
+import Counter from "./components/Counter/Counter";
+
+function App() {
+  return(
+<div>
+<TaskApp/>
+<Counter/>
+    
+</div>
+  ) 
+}
+
+export default App;
---
+++ src/components/AddTask/AddTask.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import styles from "./addTask.module.css";
+
+const AddTask = () => {
+  
+  const [value,setValue] = useState("");
+  // NOTE: do not delete `data-cy` key value pair
+  return (
+    <div className={styles.todoForm}>
+      <input data-cy="add-task-input" type="text" onChange={(e) => {
+        setValue(e.target.value)
+      }} />
+      <button data-cy="add-task-button" onClick={() => {
+        console.log(value);
+        setValue("");
+      }}>+</button>
+    </div>
+  );
+};
+
+export default AddTask;
